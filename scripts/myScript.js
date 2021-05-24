4f59b22157fe31dd7998e10e7314f5e7d450e371
Giulia-M <giuliamaggio16@gmail.com>
2021-05-24 18:15:39 +0200

    navbar -problema dropdown

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -1,10 +1,81 @@
-const app = new Vue({
+const app = new Vue ({
     el: "#app",
     data: {
-      
+        pullDownMenu: false,
+        navbarLinks: [
+            {
+                text: "Home",
+                link: "#"
+            },
+            {
+                text: "Landing",
+                link: "#",
+                elementDropdown: [
+                    {
+                    text: "Websait Hosting",
+                    link: "#"
+                    },
+                    {
+                    text: "Gaming Development",
+                    link: "#"
+                    },
+                    {
+                    text: "Cloud VPN",
+                    link: "#"
+                    }
+                ],
+                icon: "fa-chevron-down"
+            },
+            {
+                text: "Pages",
+                link: "#",
+                icon: "fa-chevron-down",
+                elementDropdown: [
+                    {
+                    text: "Web & Mobile Apps",
+                    link: "#"
+                    },
+                    {
+                        text: "Big Data Computing",
+                        link: "#"
+                    }
+                ],
+
+            },{
+                text: "Docs",
+                link: "#",
+                icon: "fa-chevron-down",
+                elementDropdown: [
+                    {
+                    text: "Video Streaming Hosting",
+                    link: "#"
+                    },
+
+                    {
+                        text: "Startups",
+                        link: "#"
+                    }
+                ],
+            },
+            
+        ],
+
+        navbarIcons: [
+            {
+                icon: ["fa-search", "fa-github", "fa-stack-overflow", "fa-user-alt"]
+            }    
+        ]
+
     },
-   
-    methods: {
-        
-    },
-});
+    methods : {
+        //NON FUNZIONA
+        clickMenu (textLink) {
+            if(!textLink.elementDropdown) {
+                return
+            }
+            this.pullDownMenu = !this.pullDownMenu;
+
+
+        }
+    }
+})
